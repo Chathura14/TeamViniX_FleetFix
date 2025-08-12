4adf6fa9a66be4b24cc3052444ae0849354fbb81
IT21810732 <it21810732@my.sliit.lk>
2025-08-13 02:46:10 +0530

    initial  commit

---
+++ backend/models/userModel.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Please enter your name'],
+    },
+    email: {
+        type: String,
+        required: [true, 'Please enter your email'],
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: [true, 'Please enter your password'],
+    },
+    role: {
+        type: String,
+        enum: ['citizen', 'driver', 'conductor', 'IT Manager'],
+        default: 'citizen',
+    },
+    NIC: { type: String },
+    drivingLicenseID: { type: String },
+    CTBID: { type: String },
+}, { timestamps: true });
+
+module.exports = mongoose.model('User', userSchema);
